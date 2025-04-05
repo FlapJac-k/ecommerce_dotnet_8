b65cdddddfebe8ca0a8d3acfa6afa0573ad8674e
FlapJac-k <eslamce122@gmail.com>
2025-04-05 23:26:46 +0200

    added search

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -34,6 +34,14 @@ public class ProductSpecParams
 
     public string? Sort { get; set; }
 
+    private string? _search;
+    public string? Search
+    {
+        get => _search ?? "";
+        set => _search = value?.ToLower();
+    }
+
+
 }
 
 
