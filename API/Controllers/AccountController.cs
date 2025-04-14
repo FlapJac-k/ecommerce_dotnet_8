ce1d8007db2cb23dcb291a1f6536e5f5db58cecd
FlapJac-k <eslamce122@gmail.com>
2025-04-14 10:34:25 +0200

    finished identity

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using API.DTOs;
+using API.Extensions;
 using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,9 +22,73 @@ namespace API.Controllers
 
             var result = await signInManager.UserManager.CreateAsync(user, registerDto.Password);
 
-            if (!result.Succeeded) return BadRequest(result.Errors);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+
+                return ValidationProblem();
+            }
 
             return Ok();
+        }
+
+        [Authorize]
+        [HttpPost("logout")]
+        public async Task<ActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+
+            return NoContent();
+        }
+
+        [HttpGet("user-info")]
+        public async Task<ActionResult> GetUserInfo()
+        {
+            if (User.Identity?.IsAuthenticated == false) return NoContent();
+
+            var user = await signInManager.UserManager.GetUserByEmailWithAddress(User);
+
+            return Ok(new
+            {
+                user.FirstName,
+                user.LastName,
+                user.Email,
+                Address = user.Address?.ToDto()
+            });
+        }
+
+        [HttpGet]
+        public ActionResult GetAuthState()
+        {
+            return Ok(new
+            {
+                IsAuthenticated = User.Identity?.IsAuthenticated ?? false
+            });
+        }
+
+        [Authorize]
+        [HttpPost("address")]
+        public async Task<ActionResult> CreateOrUpdateAddress(AddressDto addressDto)
+        {
+            var user = await signInManager.UserManager.GetUserByEmailWithAddress(User);
+
+            if (user.Address == null)
+            {
+                user.Address = addressDto.ToEntity();
+            }
+            else
+            {
+                user.Address.UpdateFromDto(addressDto);
+            }
+
+            var result = await signInManager.UserManager.UpdateAsync(user);
+
+            if (!result.Succeeded) return BadRequest("Problem updating user address");
+
+            return Ok(user.Address.ToDto());
         }
     }
 }
