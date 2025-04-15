14c6e47fb80d4349fa0ba7fb8d76e21407aefae5
FlapJac-k <eslamce122@gmail.com>
2025-04-16 01:31:49 +0200

    Updating the base controller CreatePagedResult method

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using Core.Entities.OrderAggregate;
+﻿using API.DTOs;
+using API.Extensions;
+using Core.Entities.OrderAggregate;
 using Core.Interfaces;
 using Core.Specifications;
 using Microsoft.AspNetCore.Authorization;
@@ -10,10 +12,22 @@ namespace API.Controllers
     public class AdminController(IUnitOfWork unit) : BaseApiController
     {
         [HttpGet("orders")]
-        public async Task<ActionResult<Order>> GetOrders([FromQuery] OrderSpecParams specParams)
+        public async Task<ActionResult<IReadOnlyList<OrderDto>>> GetOrders([FromQuery] OrderSpecParams specParams)
         {
             var spec = new OrderSpecification(specParams);
-            return await CreatePagedResult(unit.Repository<Order>(), spec, specParams.PageIndex, specParams.PageSize);
+            return await CreatePagedResult(unit.Repository<Order>(), spec, specParams.PageIndex, specParams.PageSize, o => o.ToDto());
+        }
+
+        [HttpGet("orders/{id:int}")]
+        public async Task<ActionResult<OrderDto>> GetOrderById(int id)
+        {
+            var spec = new OrderSpecification(id);
+
+            var order = await unit.Repository<Order>().GetEntityWithSpec(spec);
+
+            if (order == null) return BadRequest("No order with that id");
+
+            return order.ToDto();
         }
     }
 }
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -11,10 +11,21 @@ public class BaseApiController : ControllerBase
 {
     protected async Task<ActionResult> CreatePagedResult<T>(IGenericRepository<T> repo, ISpecification<T> spec, int pageIndex, int pageSize) where T : BaseEntity
     {
-        var item = await repo.ListAsync(spec);
+        var items = await repo.ListAsync(spec);
         var count = await repo.CountAsync(spec);
 
-        var pagination = new Pagination<T>(pageIndex, pageSize, count, item);
+        var pagination = new Pagination<T>(pageIndex, pageSize, count, items);
+
+        return Ok(pagination);
+    }
+
+    protected async Task<ActionResult> CreatePagedResult<T, TDto>(IGenericRepository<T> repo, ISpecification<T> spec, int pageIndex, int pageSize, Func<T, TDto> toDto) where T : BaseEntity, IDtoConvertible
+    {
+        var items = await repo.ListAsync(spec);
+        var count = await repo.CountAsync(spec);
+        var dtoItems = items.Select(toDto).ToList();
+
+        var pagination = new Pagination<TDto>(pageIndex, pageSize, count, dtoItems);
 
         return Ok(pagination);
     }
